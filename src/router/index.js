2cda834884d00a3be21d69e6d402b9e432a2755c
Muhammad Haris Firmansyah <acc.cp.haris@gmail.com>
2022-11-08 21:39:23 +0700

    Add page titles

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,58 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+const appTitle = "Pantai Teluk Sanjaya";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "Beranda",
+      component: () => import("../views/HomeView.vue"),
     },
     {
-      path: '/fasilitas',
-      name: 'fasilitas',
-      component: () => import('../views/FacilitiesView.vue')
+      path: "/fasilitas",
+      name: "Fasilitas",
+      component: () => import("../views/FacilitiesView.vue"),
     },
     {
-      path: '/hiburan',
-      name: 'hiburan',
-      component: () => import('../views/EntertainmentsView.vue')
+      path: "/hiburan",
+      name: "Hiburan",
+      component: () => import("../views/EntertainmentsView.vue"),
     },
     {
-      path: '/pesan-tiket',
-      name: 'pesan-tiket',
-      component: () => import('../views/TicketBookingView.vue')
+      path: "/pesan-tiket",
+      name: "Pesan Tiket",
+      component: () => import("../views/TicketBookingView.vue"),
     },
     {
-      path: '/tukar-kupon',
-      name: 'tukar-kupon',
-      component: () => import('../views/CouponExchangeView.vue')
+      path: "/tukar-kupon",
+      name: "Tukar Kupon",
+      component: () => import("../views/CouponExchangeView.vue"),
     },
     {
-      path: '/hubungi-kami',
-      name: 'hubungi-kami',
-      component: () => import('../views/ContactUsView.vue')
-    },,
-    {
-      path: '/masuk',
-      name: 'masuk',
-      component: () => import('../views/LoginView.vue')
-    },,
-    {
-      path: '/daftar',
-      name: 'daftar',
-      component: () => import('../views/RegisterView.vue')
+      path: "/hubungi-kami",
+      name: "Hubungi Kami",
+      component: () => import("../views/ContactUsView.vue"),
     },
-  ]
-})
+    ,
+    {
+      path: "/masuk",
+      name: "Masuk",
+      component: () => import("../views/LoginView.vue"),
+    },
+    ,
+    {
+      path: "/daftar",
+      name: "Daftar",
+      component: () => import("../views/RegisterView.vue"),
+    },
+  ],
+});
 
-export default router
+router.beforeEach((to, _, next) => {
+  document.title = `${to.name} - ${appTitle}`;
+  next();
+});
+
+export default router;

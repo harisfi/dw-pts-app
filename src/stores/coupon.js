143891d7b607bcd4a680d6c37f66975390261c9f
Muhammad Haris Firmansyah <acc.cp.haris@gmail.com>
2022-11-09 11:02:43 +0700

    Add coupon store, date formatter, and code generator

---
+++ src/stores/coupon.js
@@ -0,0 +1,52 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { getCode } from "../utils/formatter";
+
+export const useCouponStore = defineStore("coupon", () => {
+  const expDate = new Date();
+  expDate.setDate(expDate.getDate() + 1);
+
+  const ls = localStorage.getItem("coupons");
+  const initialCoupons = [
+    {
+      code: getCode(),
+      expDate: +expDate,
+      isExchanged: false,
+    },
+    {
+      code: getCode(true),
+      expDate: +expDate,
+      isExchanged: true,
+    },
+  ];
+
+  if (!ls) {
+    localStorage.setItem("coupons", JSON.stringify(initialCoupons));
+  }
+
+  const lsCoupons = JSON.parse(localStorage.getItem("coupons"));
+  const coupons = ref(lsCoupons);
+
+  function add(value) {
+    coupons.value = [...coupons.value, value];
+    localStorage.setItem("coupons", JSON.stringify(coupons.value));
+  }
+
+  function exchangeAll() {
+    let exchanged = 0;
+
+    coupons.value = coupons.value.map((c) => {
+      if (new Date(c.expDate) > new Date() && !c.isExchanged) {
+        exchanged++;
+        return { ...c, isExchanged: true };
+      } else {
+        return c;
+      }
+    });
+    localStorage.setItem("coupons", JSON.stringify(coupons.value));
+
+    return exchanged;
+  }
+
+  return { coupons, add, exchangeAll };
+});
--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -12,3 +12,28 @@ export function convertToRupiah(num) {
       .join("")
   );
 }
+
+export function formatDate(dateVal) {
+  const date = new Date(dateVal);
+  const dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("id-ID", dateOptions);
+}
+
+export function getCode(plusOne) {
+  const date = new Date();
+  const dateVal1 =
+    date.getFullYear().toString() +
+    date.getMonth().toString() +
+    date.getDate().toString();
+  const dateVal2 =
+    date.getHours().toString() +
+    date.getMinutes().toString() +
+    date.getSeconds().toString();
+  const ms = date.getMilliseconds() + (plusOne ? 1 : 0);
+  return `TIX-SJY-${dateVal1}-${dateVal2 + ms.toString()}`;
+}
